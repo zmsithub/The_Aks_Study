d54c72b2bb1da0d3fccd0ae116529d89a7f17c2b
The_AKS_circle <zmsithub@gmail.com>
2025-11-13 12:51:42 +0500

    make bajli bill

---
+++ churail.js
@@ -0,0 +1,55 @@
+// // bijli bill
+
+// // -->  100 -> 4.2 unites
+// // --> 200 -> 6 unit
+// // --> 300 -> 8 unit
+// // --> 400 ->  13 unit same rate 
+
+
+let unit = prompt("enter unit: -")
+
+let amount = 0;
+
+unit = unit *1;
+
+
+
+  if (unit > 400 ){
+   amount +=  (unit - 400) * 13;
+   unit = 400;
+
+  }
+
+
+
+  if (unit > 300){
+    amount +=  (unit - 300) * 8;
+    unit = 300;
+
+  }
+
+    if (unit > 200 ){
+    amount +=  (unit - 200) * 6;
+    unit = 200;
+
+  }
+
+    if (unit > 100 ){
+    amount +=  (unit - 100) * 4.2;
+     unit= 100;
+
+  }
+
+ if (unit < 100){
+    amount += (unit*2);
+ }
+
+  console.log( "is wakit hum puss gaya han " + amount);
+  
+// console.log(amount);
+
+
+// 480
+
+
+
